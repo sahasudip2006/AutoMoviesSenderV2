2b733c774a95932afc1bb8165584a6e1010358a9
sahasudip2006 <127957723+sahasudip2006@users.noreply.github.com>
2023-10-08 14:05:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['2729849415'])
-API_HASH = environ['0dd6c10897b85d7f10a8dcdeb74f8b8a']
-BOT_TOKEN = environ['6535249965:AAH2BM6qVqVvFJ-YyrUMV0qFJNwshn-WVP4']
+API_ID = int(environ.get('API_ID', '2729849415'))
+API_HASH = environ.get('API_HASH', '0dd6c10897b85d7f10a8dcdeb74f8b8a')
+BOT_TOKEN = environ.get('BOT_TOKEN', "6535249965:AAH2BM6qVqVvFJ-YyrUMV0qFJNwshn-WVP4")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -23,7 +23,7 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/34b8b3e4350b8a4818d5d.jpg h
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5165943027').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001953586887').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
